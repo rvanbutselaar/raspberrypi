a9afdb0df2ff5379353d3adb70e1ed86b8376c8f
Ronald van Butselaar <26540376+rvanbutselaar@users.noreply.github.com>
2021-03-01 17:08:25 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 
 sudo ln -fs /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
 sudo apt update && sudo apt upgrade -y
-sudo apt-get install vim -y
+sudo apt-get install vim docker-compose -y
 curl -sSL https://get.docker.com | sh
 sudo mkdir /data/prometheus/config
