19adf3d919679b2667c3c17bec428ad398b94377
Ronald van Butselaar <26540376+rvanbutselaar@users.noreply.github.com>
2021-03-01 17:01:33 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo ln -fs /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
 sudo apt update && sudo apt upgrade -y
 sudo apt-get install vim -y
 curl -sSL https://get.docker.com | sh
+sudo mkdir /data
