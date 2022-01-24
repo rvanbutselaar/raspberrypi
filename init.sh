bd150adc832f10218482be4cf5e51862b34f25bb
Ronald van Butselaar <ronald.van.butselaar@trivento.nl>
2022-01-24 08:28:13 +0100

    added azrt

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo ln -fs /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
 sudo apt update && sudo apt upgrade -y
-sudo apt-get install vim docker-compose -y
+sudo apt-get install vim docker-compose gnupg2 pass unattended-upgrades -y
+gpg2 --full-generate-key
 curl -sSL https://get.docker.com | sh
 sudo mkdir /data/prometheus/config
