b18bbd1d3c6b5c00850b01ab401ad292b7ad84fe
Ronald van Butselaar <ronald.van.butselaar@trivento.nl>
2022-06-10 12:52:56 +0200

    disable gpg2

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 sudo ln -fs /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
 sudo apt update && sudo apt upgrade -y
 sudo apt-get install vim docker-compose gnupg2 pass unattended-upgrades -y
-gpg2 --full-generate-key
 curl -sSL https://get.docker.com | sh
 sudo mkdir /data/prometheus/config
 echo -n ' cgroup_enable=memory swapaccount=1' >> /boot/cmdline.txt
+#gpg2 --full-generate-key
