63f89846162d19de4a23ed65664b87c3f9f42e49
Ronald van Butselaar <ronald.van.butselaar@trivento.nl>
2022-06-10 12:53:47 +0200

    recursive create

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ sudo ln -fs /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
 sudo apt update && sudo apt upgrade -y
 sudo apt-get install vim docker-compose gnupg2 pass unattended-upgrades -y
 curl -sSL https://get.docker.com | sh
-sudo mkdir /data/prometheus/config
+sudo mkdir -p /data/prometheus/config
 echo -n ' cgroup_enable=memory swapaccount=1' >> /boot/cmdline.txt
 #gpg2 --full-generate-key
