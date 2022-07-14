0ba00ea83714a5481cd6892be52004f250e64464
Ronald van Butselaar <ronald.van.butselaar@trivento.nl>
2022-07-14 11:05:01 +0200

    thanos s3 long term storage

--- init.sh
+++ init.sh
@@ -4,5 +4,6 @@ sudo apt update && sudo apt upgrade -y
 sudo apt-get install vim docker-compose gnupg2 pass unattended-upgrades -y
 curl -sSL https://get.docker.com | sh
 sudo mkdir -p /data/prometheus/config
+sudo mkdir -p /data/thanos/config
 echo -n ' cgroup_enable=memory swapaccount=1' >> /boot/cmdline.txt
 #gpg2 --full-generate-key
